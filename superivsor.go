bdc5e350dcc9ee0e7ab7a01f30ea4e153312f069
Nick Duskey <nduskey@smartlink.city>
2019-09-11 13:40:53 -0400

    Add rudimentary logger

--- superivsor.go
+++ superivsor.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func startProgram(path string) error {
-	cmd := exec.Command("cmd.exe", "/C", "start", "/b", path)
+	cmd := exec.Command("cmd.exe", "/C", "start", path)
 	err := cmd.Run()
 	return err
 }
 
 func supervise() {
+	rootDir := os.Getenv("SYSTEMDRIVE") + "\\"
+	logPath := fmt.Sprintf("%swindsupervisor\\error.log", rootDir)
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("error opening file: %v\n", err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
 	programs, err := loadPrograms()
 	if err != nil {
 		log.Fatalf("error fetching programs: %v\n", err)
